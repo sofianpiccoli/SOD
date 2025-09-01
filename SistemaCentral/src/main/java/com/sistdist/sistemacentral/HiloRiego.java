85259bd226933a800aaebbe8414c76dd1ad35a24
miria <miria@192.168.0.18>
2025-08-31 22:22:05 -0300

    agregando las humedades de cada sensor al hashmap

--- SistemaCentral/src/main/java/com/sistdist/sistemacentral/HiloRiego.java
+++ SistemaCentral/src/main/java/com/sistdist/sistemacentral/HiloRiego.java
@@ -4,27 +4,33 @@
  */
 package com.sistdist.sistemacentral;
 
+import java.io.PrintWriter;
+import java.util.Map;
+
 /**
  *
  * @author sofianietopiccoli
  */
 public class HiloRiego extends Thread{
-        private double w1;
-        private double w2;
-        private double w3;
-        private boolean on;
-        
-        private HiloDatosCompartidos datos;
-        private HiloHumedad hum;
-       
-    public HiloRiego(double w1, double w2, double w3, HiloDatosCompartidos datos, HiloHumedad hum){
+    private double w1, w2, w3;
+    private boolean on;
+    private HiloDatosCompartidos datos;
+    private Map<Integer, HiloHumedad> humedades;
+    private Map<Integer, PrintWriter> valvulas;
+
+    public HiloRiego(double w1, double w2, double w3, 
+                     HiloDatosCompartidos datos,
+                     Map<Integer, HiloHumedad> humedades) {
         this.w1 = w1;
         this.w2 = w2;
         this.w3 = w3;
         this.datos = datos;
-        this.hum = hum;
-        on = false;
-        
+        this.humedades = humedades;
+        this.on = true;
+    }
+
+    public void setValvulas(Map<Integer, PrintWriter> valvulas) {
+        this.valvulas = valvulas;
     }
     public double calculoINR(double H, double T, double R){
         double inr = w1*(1-H/100) + w2*(T/40) + w3*(R/1000);
@@ -47,9 +53,9 @@ public class HiloRiego extends Thread{
         return minutos;
     }
     
-    public void abrirValvula(int valvula, int tiempo){
+    /*public void abrirValvula(int valvula, int tiempo){
         System.out.println("Parcela "+valvula+" regándose por "+tiempo+" minutos");
-    }
+    }*/
     
     public void apagar(){
         on = false;
@@ -57,7 +63,37 @@ public class HiloRiego extends Thread{
     
     
         @Override
-    public void run(){
+    public void run() {
+        while (on) {
+            for (int parcela : humedades.keySet()) {
+                double H = humedades.get(parcela).getHumedad();
+                double inr = calculoINR(H, datos.getTemperatura(), datos.getRadiacion());
+                System.out.println("INR parcela " + parcela + " = " + inr);
+
+                if (decidirRiego(datos.isLluvia(), inr)) {
+                    int minutos = tiempoRiego(inr);
+                    abrirValvula(parcela, minutos);
+                }
+            }
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void abrirValvula(int parcela, int tiempo) {
+        if (valvulas != null && valvulas.containsKey(parcela)) {
+            PrintWriter pw = valvulas.get(parcela);
+            pw.println("TIEMPO=" + tiempo);
+            pw.flush();
+            System.out.println("Orden enviada -> Parcela " + parcela + " regando " + tiempo + " minutos");
+        }
+    }
+}
+    //public void run(){
         /*on = true;
         while (on){
             for (int i = 1; i <=5; i++){
@@ -86,5 +122,5 @@ public class HiloRiego extends Thread{
                 System.getLogger(HiloRiego.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
             }
         }*/
-    }
-}
+   // }
+
--- SistemaCentral/src/main/java/com/sistdist/sistemacentral/SistemaCentral.java
+++ SistemaCentral/src/main/java/com/sistdist/sistemacentral/SistemaCentral.java
@@ -7,8 +7,11 @@ package com.sistdist.sistemacentral;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -18,32 +21,49 @@ public class SistemaCentral {
 
     public static void main(String[] args) {
         String tipoDispositivo = " ";
+        Map<Integer, HiloHumedad> humedades = new HashMap<>();
+        Map<Integer, PrintWriter> valvulas = new HashMap<>();
+        HiloDatosCompartidos datos = new HiloDatosCompartidos();
         try {
             ServerSocket server = new ServerSocket(20000);
             while (true){
                 Socket s = server.accept(); //puerto aleatorio misma dir de internet local
+                // Arranco el hilo de riego una vez
+                HiloRiego riego = new HiloRiego(0.5, 0.3, 0.2, datos, humedades);
+                riego.setValvulas(valvulas);
+                riego.start();
                 BufferedReader bf = new BufferedReader(new InputStreamReader(s.getInputStream()));
                 tipoDispositivo = bf.readLine();
                 switch(tipoDispositivo){
                     case "sensorHumedad1":
+                        System.out.println("soy el sensor de humedad 1");
                         HiloHumedad hum1 = new HiloHumedad(s);
                         hum1.start();
+                        humedades.put(1, hum1); 
                         break;
                     case "sensorHumedad2":
+                        System.out.println("soy el sensor de humedad 2");
                         HiloHumedad hum2 = new HiloHumedad(s);
                         hum2.start();
+                        humedades.put(2, hum2); 
                         break;
                     case "sensorHumedad3":
+                        System.out.println("soy el sensor de humedad 3");
                         HiloHumedad hum3 = new HiloHumedad(s);
                         hum3.start();
+                        humedades.put(3, hum3); 
                         break;
                     case "sensorHumedad4":
+                        System.out.println("soy el sensor de humedad 4");
                         HiloHumedad hum4 = new HiloHumedad(s);
                         hum4.start();
+                        humedades.put(4, hum4); 
                         break;  
                     case "sensorHumedad5":
+                        System.out.println("soy el sensor de humedad 5");
                         HiloHumedad hum5 = new HiloHumedad(s);
                         hum5.start();
+                        humedades.put(5, hum5); 
                         break;
                     case "sensorTemperatura":
                         HiloTemperatura tem = new HiloTemperatura(s);
@@ -57,10 +77,33 @@ public class SistemaCentral {
                         HiloRadiacion rad = new HiloRadiacion(s);
                         rad.start();
                         break;
-                }
-                  
-            }
-        } catch (IOException ex) {
+                    case "HelectroValvula1":
+                        System.out.println("soy la electrovalvula 1");
+                       PrintWriter pw1 = new PrintWriter(s.getOutputStream(), true);
+                       valvulas.put(1, pw1);
+                       break;
+                    case "HelectroValvula2":
+                        System.out.println("soy la electrovalvula 2");
+                       PrintWriter pw2 = new PrintWriter(s.getOutputStream(), true);
+                       valvulas.put(2, pw2);
+                       break;
+                    case "HelectroValvula3":
+                        System.out.println("soy la electrovalvula 3");
+                       PrintWriter pw3 = new PrintWriter(s.getOutputStream(), true);
+                       valvulas.put(3, pw3);
+                       break;
+                    case "HelectroValvula4":
+                        System.out.println("soy la electrovalvula 4");
+                       PrintWriter pw4 = new PrintWriter(s.getOutputStream(), true);
+                       valvulas.put(4, pw4);
+                       break;
+                    case "HelectroValvula5":
+                        System.out.println("soy la electrovalvula 5");
+                       PrintWriter pw5 = new PrintWriter(s.getOutputStream(), true);
+                       valvulas.put(5, pw5);
+                       break;
+
+                }}} catch (IOException ex) {
             System.getLogger(SistemaCentral.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
         
